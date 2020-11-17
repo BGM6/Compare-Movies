0a0b381da69b375700f2b0031b12c356e9ab15bf
bguilas <bryanjguilas@gmail.com>
2020-11-16 22:51:19 -0800

    rendered movie poster and movie title on the webpage

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 // const apiKey = 5e970758
 
 const fetchData = async (searchTerm) => {
-
     let response = await axios.get('https://www.omdbapi.com/?', {
         params: {
             apikey: '5e970758',
             s: searchTerm,
         }
     })
-    console.log(response.data);
+    return response.data.Search;
 };
 
-input = document.querySelector('input');
-
-const onInput = event => {
-    fetchData(event.target.value);
+const input = document.querySelector('input');
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    for (let movie of movies) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="${movie.Poster}" alt="movie poster" />
+            <h1>${movie.Title}</h1>
+     `;
+        document.querySelector('#target').appendChild(div);
+    }
 };
 input.addEventListener('input', debounce(onInput, 500));
 

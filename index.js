4caee7d4d3af515d34087656f3ff9d26c285f563
bguilas <bryanjguilas@gmail.com>
2020-11-16 22:32:14 -0800

    created utils.js

--- index.js
+++ index.js
@@ -13,19 +13,6 @@ const fetchData = async (searchTerm) => {
 
 input = document.querySelector('input');
 
-const debounce = (func) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func.apply(null, args);
-        }, 1000);
-    };
-};
-
-
 const onInput = event => {
     fetchData(event.target.value);
 };

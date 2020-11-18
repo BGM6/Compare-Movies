c680e4fd04cba8ed7f3a6416115d9dfd3d5e0916
bguilas <bryanjguilas@gmail.com>
2020-11-17 18:35:45 -0800

    worked on dropdown

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // const apiKey = 5e970758
-
 const fetchData = async (searchTerm) => {
     let response = await axios.get('https://www.omdbapi.com/?', {
         params: {
@@ -15,6 +14,7 @@ const fetchData = async (searchTerm) => {
 };
 
 const root = document.querySelector('.autocomplete');
+
 root.innerHTML = `
 <label><b>Search For a Movie</b></label>
 <input class="input" />
@@ -30,20 +30,32 @@ const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
-
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
 
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
+
     for (let movie of movies) {
         const option = document.createElement('a');
-
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         option.classList.add('dropdown-item');
         option.innerHTML = `
-            <img src="${movie.Poster}" alt="movie poster" />
+            <img src="${imgSrc}" alt="movie poster" />
             ${movie.Title}
      `;
         resultsWrapper.appendChild(option);
     }
 };
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
